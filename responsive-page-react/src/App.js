d72fa346fa7c1d712733ef236a57a73e1a75b3b2
Vishwajeet Gour <137927385+GourVishwajeet@users.noreply.github.com>
2024-06-16 09:31:12 +0000

    npm install

---
+++ responsive-page-react/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <div>
+
+    hello world
+    <div>
+      hello 
+      </div>
+   </div>
+    </div>
+  );
+}
+
+export default App;
